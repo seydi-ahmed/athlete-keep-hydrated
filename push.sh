988fd2c0ce6205d8fb33ae25119f30520d43f48f
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-08 17:06:58 +0000

    user interview done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "storyboard"
+git commit -m "user interview done"
 git push
