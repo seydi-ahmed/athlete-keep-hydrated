281dc32e48fe40dc151cdd34df0a4d051fd8eb7e
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-08 17:33:07 +0000

    Persona de Pierre presque done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Analytics done"
+git commit -m "Persona de Pierre presque done"
 git push
