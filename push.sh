3f09d6722f541077cdf04e6f6cd8cbbe583ec808
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-08 17:41:23 +0000

    User Journey presque done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Persona de Pierre presque done"
+git commit -m "User Journey presque done"
 git push
