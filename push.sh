eebe4e56bd01b36180708cbe78138d2c6181d73d
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-09 15:02:51 +0000

    to_do file updated

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "ideation and storyboard folders created"
+git commit -m "to_do file updated"
 git push
