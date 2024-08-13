a87b54e407796ccab18f9a41c492103e4f9da009
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-13 21:15:54 +0000

    picture for storyboard added

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Readme and to_do.txt updated"
+git commit -m "picture for storyboard added"
 git push
