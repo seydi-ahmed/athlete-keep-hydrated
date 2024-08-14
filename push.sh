26a113f9557e871641114d9aa98e05ad73259efe
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-14 19:12:04 +0000

    1 pictures restantes

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "5 pictures added"
+git commit -m "1 pictures restantes"
 git push
