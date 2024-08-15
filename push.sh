fa421d2283808c8b18ec8c7dfe471f630f9492ad
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-15 17:43:50 +0000

    jpg to pdf userJourney

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "1 pictures restantes"
+git commit -m "jpg to pdf userJourney"
 git push
