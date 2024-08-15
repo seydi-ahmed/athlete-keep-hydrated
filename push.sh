ba47fc2bec91457104a2e2f7bb84bbb8d28a89b2
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-15 18:29:34 +0000

    avant derniére partie done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "jpg to pdf userJourney"
+git commit -m "avant derniére partie done"
 git push
