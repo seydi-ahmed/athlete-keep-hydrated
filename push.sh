9646162e44236185c90898f924dde67e4e08efa4
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-18 17:44:22 +0000

    user journey done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "how to manage the penultimate session?"
+git commit -m "user journey done"
 git push
