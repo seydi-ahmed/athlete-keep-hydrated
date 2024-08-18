cbbf27d5b02f2862daf37de651b5727141194743
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-18 17:35:43 +0000

    how to manage the last session

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "avant derniére partie done"
+git commit -m "how to manage the last session"
 git push
