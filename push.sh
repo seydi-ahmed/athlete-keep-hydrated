9cf47847af61f4dab589f7c14a2a7d19af7ba08d
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-18 17:54:24 +0000

    brainstorming updated

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "user journey done"
+git commit -m "brainstorming updated"
 git push
