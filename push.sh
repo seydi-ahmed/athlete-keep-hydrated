8365b091737111f1d4abdecdb0bf7d6919600823
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-18 17:58:54 +0000

    there remains the practical part.

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "brainstorming updated"
+git commit -m "there remains the practical part."
 git push
