6eab15bcf667da52e279363ef645f2cac2140284
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-20 18:12:38 +0000

    storyboard reset

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "there remains the practical part."
+git commit -m "storyboard reset"
 git push
