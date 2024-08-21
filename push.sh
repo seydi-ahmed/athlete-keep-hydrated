0e8c1540c6adbbe910e334a040b55d8c34325d91
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-21 16:58:05 +0000

    figma done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "ideation done zip"
+git commit -m "figma done"
 git push
